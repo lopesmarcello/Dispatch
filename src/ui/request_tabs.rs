828f3b0b6f3c1a8284b4ba59c8b5c8a3e0a946b7
Marcello Lopes <marcellolopesdev@gmail.com>
2026-01-22 13:25:13 -0300

    JSON highlighting

--- src/ui/request_tabs.rs
+++ src/ui/request_tabs.rs
@@ -1,37 +1,46 @@
-use crate::{config, ui::key_value_editor::KeyValueEditor};
-use gtk::{Box, Label, Notebook, Orientation, ScrolledWindow, prelude::*};
-use sourceview5::{Buffer, StyleSchemeManager, View, prelude::*};
+use crate::{config, ui::helpers, ui::key_value_editor::KeyValueEditor};
+use gtk::{prelude::*, Box, Label, Notebook, Orientation, ScrolledWindow};
+use sourceview5::prelude::*;
 
-pub fn build() -> (Notebook, Buffer, KeyValueEditor) {
+pub fn build() -> (Notebook, sourceview5::Buffer, KeyValueEditor) {
     let notebook = Notebook::new();
     notebook.set_vexpand(true);
 
-    let body_box = Box::new(Orientation::Vertical, config::SPACING_NONE);
+    fn create_json_view() -> (ScrolledWindow, sourceview5::Buffer) {
+        let buffer = sourceview5::Buffer::new(None);
 
-    let buffer = Buffer::new(None);
-    let style_manager = StyleSchemeManager::default();
-    if let Some(s) = style_manager
-        .scheme(config::EDITOR_SCHEME_PREF_1)
-        .or_else(|| style_manager.scheme(config::EDITOR_SCHEME_PREF_2))
-        .or_else(|| style_manager.scheme(config::EDITOR_SCHEME_PREF_3))
-    {
-        buffer.set_style_scheme(Some(&s));
+        let style_manager = sourceview5::StyleSchemeManager::default();
+        let scheme = style_manager
+            .scheme(config::EDITOR_SCHEME_PREF_1)
+            .or_else(|| style_manager.scheme(config::EDITOR_SCHEME_PREF_2))
+            .or_else(|| style_manager.scheme(config::EDITOR_SCHEME_PREF_3));
+
+        if let Some(s) = scheme {
+            buffer.set_style_scheme(Some(&s));
+        }
+
+        // Set JSON syntax highlighting
+        helpers::set_syntax_highlighting(&buffer, "application/json");
+
+        let view = sourceview5::View::with_buffer(&buffer);
+        view.set_monospace(true);
+        view.set_show_line_numbers(true);
+        view.set_top_margin(config::SPACING_MEDIUM);
+        view.set_bottom_margin(config::SPACING_MEDIUM);
+        view.set_left_margin(config::SPACING_MEDIUM);
+
+        let scrolled_window = ScrolledWindow::builder()
+            .hscrollbar_policy(gtk::PolicyType::Automatic)
+            .vscrollbar_policy(gtk::PolicyType::Automatic)
+            .child(&view)
+            .vexpand(true)
+            .build();
+
+        (scrolled_window, buffer)
     }
 
-    let view = View::with_buffer(&buffer);
-    view.set_monospace(true);
-    view.set_show_line_numbers(true);
-    view.set_top_margin(config::SPACING_MEDIUM);
-    view.set_left_margin(config::SPACING_MEDIUM);
-    view.set_bottom_margin(config::SPACING_MEDIUM);
-
-    let scrolled = ScrolledWindow::builder()
-        .hscrollbar_policy(gtk::PolicyType::Automatic)
-        .vscrollbar_policy(gtk::PolicyType::Automatic)
-        .child(&view)
-        .vexpand(true)
-        .build();
-
+    let body_box = Box::new(Orientation::Vertical, config::SPACING_NONE);
+    let (scrolled, buffer) = create_json_view();
     body_box.append(&scrolled);
 
     let tab_label = Label::new(Some("Body (JSON)"));
